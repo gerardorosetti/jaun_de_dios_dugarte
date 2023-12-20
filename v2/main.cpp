e306a29c487ba44071b07fc65b179327dc1fc188
Saymed Dustga <gasayagom@gmail.com>
2023-12-20 17:27:49 -0400

    start working

--- v2/main.cpp
+++ v2/main.cpp
@@ -49,13 +49,29 @@ std::string to_lower(std::string s)
     return result;
 }
 
+bool check_only_numbers(std::string s)
+{
+    bool result = true;
+
+    for (size_t i = 0; i < s.length(); ++i)
+    {
+        if (s[i] < 48 || s[i] > 57)
+        {
+            result = false;
+            break;
+        }
+    }
+    
+    return result;
+}
+
 std::string only_letters(std::string s)
 {
     std::string result = "";
 
     for (size_t i = 0; i < s.length(); ++i)
     {
-        if (std::regex_match(std::string{s[i]}, regexp) || s[i] == ' ')
+        if ((std::regex_match(std::string{s[i]}, regexp) || s[i] == ' ') && s[i] != '-')
         {
             result += s[i];
         }
@@ -127,6 +143,8 @@ void print_questions_vector_data(std::vector<Question>& questions)
 
 int main ()
 {
+    // clear();
+
     //Allow especial chars
     setlocale(LC_ALL, "");
 
@@ -140,9 +158,12 @@ int main ()
 
     Question q{"", "", std::vector<std::string>{}};
 
+    std::vector<std::string> initial_menu_lines{""};
+
     //Getting the questions
     while(!data_file.eof())
     {
+        
         std::string line;
         std::getline(data_file, line);
         if (line.length() == 0)
@@ -163,7 +184,8 @@ int main ()
         }
         if(menu_flag)
         {
-            std::cout << line << std::endl;
+            // std::cout << line << std::endl;
+            initial_menu_lines.push_back(line);
             continue;
         }
 
@@ -241,13 +263,191 @@ int main ()
         }
     }
 
-    clear();
-
     questions.push_back(q);
 
     data_file.close();
 
-    print_questions_vector_data(questions);
+    auto print_initial_menu = [&initial_menu_lines] ()
+    {
+        clear();
+
+        std::cout << "Bienvenido al Asistente de Estudio de\n\t    Juan Dugarte" << std::endl;
+        std::cout << std::endl;
+        for (std::string line : initial_menu_lines)
+        {
+            std::cout << line << std::endl;
+        }
+        std::cout << std::endl;
+    };
+
+
+    // print_questions_vector_data(questions);
+    auto print_no_valid_option_message = [] ()
+    {
+        clear();
+        std::cout << "\n\n\tOpcion no valida, intente nuevamente\n\n";
+        std::this_thread::sleep_for(2s);
+    };
+
+    do
+    {
+        print_initial_menu();
+        std::cout << "Seleccione de que manera le gustaria trabajar:" << std::endl;
+        std::cout << "1) Modo Ordenado\n2) Modo Aleatorio\n3) Salir\n\nIngrese su opcion [1, 2 o 3]: ";
+        std::string option;
+        std::cin >> option;
+        if (option == "1")
+        {
+            clear();
+            // print_initial_menu();
+            std::getchar();
+            std::string start{""}, end{""};
+
+            do
+            {
+                print_initial_menu();
+                std::cout << "Seleccione con cual pregunta desea empezar: ";
+                std::cin >> start;
+                if (check_only_numbers(start))
+                {
+                    break;
+                }
+                print_no_valid_option_message();
+            } while (true);
+
+            clear();
+
+            do
+            {
+                print_initial_menu();
+                std::cout << "Seleccione con cual pregunta desea terminar: ";
+                std::cin >> end;
+                if (check_only_numbers(end))
+                {
+                    break;
+                }
+                print_no_valid_option_message();
+            } while (true);
+
+            size_t start_index = std::stoi(start) - 1;
+            size_t end_index = std::stoi(end);
+
+            std::getchar();
+
+            for (size_t index = start_index; index < end_index; ++index)
+            {
+                clear();
+                Question q = questions[index];
+                std::cout << index + 1 << ") " << q.title << std::endl << q.hint << std::endl << "\nTu Respuesta: ";
+                std::string ans;
+                std::getline(std::cin, ans);
+                // std::vector<std::string> ans_vec{split(only_letters(to_lower(ans)))};
+                
+                size_t words_match_counter = 0;
+
+                auto check_sentences_match = [&words_match_counter] (std::string s1, std::string s2)
+                {
+                    std::vector<std::string> v1{split(only_letters(to_lower(s1)))};
+                    std::vector<std::string> v2{split(only_letters(to_lower(s2)))};
+                    bool result = true;
+                    for (std::string s1_temp : v1)
+                    {
+                        if (std::any_of(v2.begin(), v2.end(), [&s1_temp] (std::string s2_temp) { return s1_temp == s2_temp; }))
+                        {
+                            // result = false;
+                            // break;
+                            ++words_match_counter;
+                        }
+                    }
+                    return words_match_counter == v2.size();
+                };
+
+                std::string big_ans = "";
+
+                for (std::string ans_item : q.answer)
+                {
+                    big_ans += ans_item + ' ';
+                }
+
+                big_ans.pop_back();
+
+                if (!check_sentences_match(ans, big_ans))
+                {
+                    std::cout << "\n\nIncorrecto... Numero de palabras acertadas: " << words_match_counter << " / " << split(big_ans).size() << std::endl;
+                    std::cout << "\n\nRespuesta esperada: " << big_ans << std::endl;
+                }
+                else std::cout << "\n\nExcelente!!! Numero de palabras acertadas: " << words_match_counter << " / " << split(big_ans).size() << std::endl;
+
+
+                std::string xd;
+                // std::this_thread::sleep_for(3s);
+                std::cin>> xd;
+                break;
+                
+
+                // if (std::all_of(q.answer.begin(), q.answer.end(), [&ans_vec] (std::string value) { return std::any_of(ans) }))
+                // {
+                //     /* code */
+                // }
+                
+
+            }
+        }
+        else if(option == "2")
+        {
+
+            std::random_device rd{};
+            std::mt19937_64 rng(rd());
+
+            std::getchar();
+
+            std::vector<Question> questions_temp{questions};
+            bool again = false;
+            Question q;
+            for (size_t index = 0; questions_temp.size() > 0; ++index)
+            {
+                clear();
+                std::uniform_int_distribution<size_t> dis{0, questions_temp.size() - 1};
+                size_t num = dis(rng);
+                if (!again)
+                {
+                    q = questions_temp[num];
+                    std::vector<Question> q_temp_vec{};
+                    Question q_aux;
+                    while (true)
+                    {
+                        Question q_aux = questions_temp[questions_temp.size() - 1];
+                        questions_temp.pop_back();
+                        if (q_aux.title == q.title && q_aux.answer == q.answer && q.hint == q_aux.hint)
+                        {
+                            for (Question temp : q_temp_vec)
+                            {
+                                questions_temp.push_back(temp);
+                            }
+                            break;
+                        }
+                        q_temp_vec.push_back(q_aux);
+                    }
+                }
+                std::cout << index + 1 << ") " << q.title << std::endl << q.hint << std::endl << "\nTu Respuesta: ";
+                std::string ans;
+                std::getline(std::cin, ans);
+               
+            }
+        }
+        else if(option == "3")
+        {
+            break;
+        }
+        else
+        {
+            // clear();
+            // std::cout << "\n\n\tOpcion no valida, intente nuevamente\n\n";
+            // std::this_thread::sleep_for(2s);
+            print_no_valid_option_message();
+        }
+    } while (true);
+    
 
     return EXIT_SUCCESS;
 }
